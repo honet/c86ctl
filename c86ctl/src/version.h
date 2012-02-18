13e74326492860e5456b87c950876e049ea74635
kkotajima <honet.kk@gmail.com>
2012-02-18 13:32:36 +0900

    バージョン上げ忘れ。

--- c86ctl/src/version.h
+++ c86ctl/src/version.h
@@ -11,9 +11,9 @@
 #define VERSION_MAJOR      1
 #define VERSION_MINOR      3
 #define VERSION_REVISION   0
-#define VERSION_BUILD      14
-#define VERSION_STRING     "1, 3, 0, 14"
-#define VERSION_MESSAGE    "VER 1.3.0.14(DBG-ONLY)"
+#define VERSION_BUILD      15
+#define VERSION_STRING     "1, 3, 0, 15"
+#define VERSION_MESSAGE    "VER 1.3.0.15(DBG-ONLY)"
 
 
 // 以下はDLLリソース用定義につき変更禁止
